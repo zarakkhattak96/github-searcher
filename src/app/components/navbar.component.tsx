ddb32237f3d061e25f112df8f2e7245d496db504
Zarak Khattak <134920351+zarakkhattak96@users.noreply.github.com>
2024-04-04 00:39:46 +0500

    init git-searcher design with fetch api

--- src/app/components/navbar.component.tsx
+++ src/app/components/navbar.component.tsx
@@ -1,13 +1,3 @@
-
-export default function NavBar(){
-
-    return <>
-    
-    <div className="navbar"> NavBar
-
-    <img src="../public/github-logo.png" alt="github-logo" className="github-logo"/>    
-    
-    </div>
-
-    </>
+export default function NavBar() {
+  return <></>;
 }
--- src/app/components/search.component.tsx
+++ src/app/components/search.component.tsx
@@ -1,23 +1,49 @@
-import React, { useState } from "react"
-
+import { useState } from 'react';
+import { Flex, Input, Dropdown, Space, Button, Select } from 'antd';
+import type { MenuProps } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 
 export default function Search() {
-    
-    const [userName, setUserName] = useState("")
+  const [userName, setUserName] = useState('');
 
+  const userUrl = `https://api.github.com/users/${userName}`;
+  const repoUrl = `https://api.github.com/users/${userName}/repos`;
 
- 
+  const items: MenuProps['items'] = [
+    {
+      label: <a href={userUrl}>Users</a>,
+      key: '0',
+    },
+    {
+      label: <a href={repoUrl}>Repositories</a>,
+      key: '1',
+    },
+  ];
 
-    return (
-    
+  return (
     <>
-    <h1> GitHub Searcher</h1>
-    <input type="text" value={userName} onChange={(e)=> setUserName(e.target.value)}/>
-    <button type="button"> User</button>
-      
+      <Space>
+        <GithubOutlined className='github-logo' />
+
+        <h2 className='title'>Github Search</h2>
+      </Space>
+      <Flex vertical gap={12}>
+        <Input
+          type='text'
+          placeholder='Start typing to search ..'
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          className='search-input'
+        />
+
+        <Select className='dropdown-menu' placeholder='Select an option'>
+          <Dropdown menu={{ items }} trigger={['click']}>
+            <Space>
+              <Button className='button'> User</Button>
+            </Space>
+          </Dropdown>
+        </Select>
+      </Flex>
     </>
-      
-      
-      
-      )
+  );
 }
