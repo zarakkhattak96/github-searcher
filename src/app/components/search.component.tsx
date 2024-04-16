3d20ae8720c14815738ddcafe568fbbddb53953b
Zarak Khattak <134920351+zarakkhattak96@users.noreply.github.com>
2024-04-16 23:39:20 +0500

    update code with rows and columns

--- src/app/components/search.component.tsx
+++ src/app/components/search.component.tsx
@@ -1,15 +1,17 @@
-import { UserOutlined } from '@ant-design/icons';
-import { Flex, Input, Select, Avatar, Space, Typography, Tabs } from 'antd';
+import { Flex, Input, Select, Typography, Row, Col, Image, Space } from 'antd';
 import type { TabsProps } from 'antd';
 import Card from 'antd/es/card/Card';
 
 import { useState } from 'react';
 import { debounce } from '../../global/debounce';
+import Meta from 'antd/es/card/Meta';
 
 const { Title } = Typography;
+// const {Meta} = Card;
+
 export default function Search() {
   const [username, setUsername] = useState('');
-  const [userProfile, setUserProfile] = useState({});
+  const [userProfile, setUserProfile] = useState([]);
 
   const [userFollowers, setUserFollowers] = useState([]);
   const [userRepos, setUserRepos] = useState([]);
@@ -39,7 +41,7 @@ export default function Search() {
     },
   ];
 
-  const [selectedTab, setSelectedTab] = useState('followers');
+  // const [selectedTab, setSelectedTab] = useState('followers');
 
   const searchProfile = async () => {
     const response = await fetch(
@@ -48,7 +50,9 @@ export default function Search() {
 
     const data = await response?.json();
 
-    setUserProfile(data.items[0]);
+    if (data.items.length > 0) {
+      setUserProfile([...userProfile, data.items[0]]);
+    }
     await fetchUserFollowers();
     await fetchUserRepos();
   };
@@ -71,21 +75,75 @@ export default function Search() {
     setUserRepos(data);
   };
 
-  const handleTabChange = async (key: string) => {
-    setSelectedTab(key);
+  // const handleTabChange = async (key: string) => {
+  //   setSelectedTab(key);
 
-    if (key === '1') {
-      await fetchUserFollowers(userProfile.followers_url);
-    } else if (key === '2') {
-      await fetchUserRepos(userProfile.repos_url);
-    }
-  };
+  //   if (key === '1') {
+  //     await fetchUserFollowers(userProfile.followers_url);
+  //   } else if (key === '2') {
+  //     await fetchUserRepos(userProfile.repos_url);
+  //   }
+  // };
 
   const debouncedProfileSearch = debounce(searchProfile, 1000);
 
   return (
     <>
-      <Flex vertical gap='middle' wrap='wrap'>
+      <div>
+        <Flex vertical gap='middle' wrap='wrap'>
+          <Input
+            placeholder='Start typing here ..'
+            maxLength={50}
+            size='large'
+            type='text'
+            style={{
+              borderRadius: '0',
+              height: 'auto',
+              width: '100%',
+              maxWidth: '500px',
+            }}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <Select
+            placeholder='User'
+            options={[{ value: 'user', label: 'User' }]}
+            style={{
+              width: '30%',
+              height: 'auto',
+              // left: '380px',
+              // bottom: '55px',
+            }}
+            size='large'
+            onClick={debouncedProfileSearch}
+          />
+
+          <Row gutter={[96, 6]}>
+            {userProfile.map((profile, index) => (
+              <Col key={index} span={8}>
+                <Card
+                  hoverable
+                  style={{ width: 240 }}
+                  cover={<Image alt='user dp' src={profile.avatar_url} />}
+                >
+                  <Meta title={profile.login} description={profile.html_url} />
+
+                  <div>
+                    <h3>Followers</h3>
+
+                    {userFollowers.map((follower) => (
+                      <div key={follower.id}>{follower.login}</div>
+                    ))}
+                  </div>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Flex>
+      </div>
+
+      {/* <Flex vertical gap='middle'>
         <Input
           placeholder='Start typing here ..'
           maxLength={50}
@@ -115,41 +173,48 @@ export default function Search() {
         />
       </Flex>
 
-      {userProfile.login !== undefined && (
-        <Card style={{ marginTop: '20px', maxWidth: '400px', height: 'auto' }}>
-          <div>
-            <Space direction='vertical' size={16}>
-              <Avatar
-                size={64}
-                icon={<UserOutlined />}
-                src={userProfile?.avatar_url}
-                alt='User Profile'
-                style={{
-                  marginTop: '10px',
-                  marginBottom: '10px',
-                  height: '150px',
-                  width: '150px',
-                }}
-              />
-              <Title level={4}>{userProfile?.login}</Title>
-            </Space>
+      <Row gutter={[16, 16]}>
+        {userProfile.login !== undefined && (
+          <Col span={24}>
+            <Card
+              hoverable
+              style={{ marginTop: '20px', maxWidth: '400px', height: 'auto' }}
+            >
+              <div>
+                <Space direction='vertical' size={16}>
+                  <Avatar
+                    size={64}
+                    icon={<UserOutlined />}
+                    src={userProfile?.avatar_url}
+                    alt='User Profile'
+                    style={{
+                      marginTop: '10px',
+                      marginBottom: '10px',
+                      height: '150px',
+                      width: '150px',
+                    }}
+                  />
+                  <Title level={4}>{userProfile?.login}</Title>
+                </Space>
 
-            <>
-              <Tabs
-                activeKey={selectedTab}
-                onChange={handleTabChange}
-                destroyInactiveTabPane={true}
-                onTabClick={(key) => handleTabChange(key)}
-                items={items}
-              >
-                <Tabs.TabPane tab='Followers' key='1' />
+                <>
+                  <Tabs
+                    activeKey={selectedTab}
+                    onChange={handleTabChange}
+                    destroyInactiveTabPane={true}
+                    onTabClick={(key) => handleTabChange(key)}
+                    items={items}
+                  >
+                    <Tabs.TabPane tab='Followers' key='1' />
 
-                <Tabs.TabPane tab='Repositories' key='2' />
-              </Tabs>
-            </>
-          </div>
-        </Card>
-      )}
+                    <Tabs.TabPane tab='Repositories' key='2' />
+                  </Tabs>
+                </>
+              </div>
+            </Card>
+          </Col>
+        )}
+      </Row> */}
     </>
   );
 }
